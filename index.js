216740d0d6857db6cbe9c7f8c72c8908537ca5a5
Daniela Noir <daniela@danielanoir.com>
2016-09-13 14:09:35 -0400

    broken code

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var config = require('../config/uber_config');
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
-var TYPES = require('tedious').TYPES;
+// var Connection = require('tedious').Connection;
+// var Request = require('tedious').Request;
+// var TYPES = require('tedious').TYPES;
 var data = new String();
 var json = new Object();
-var connection = new Connection(config.db)
+// var connection = new Connection(config.db)
+var hbs = require("hbs");
+// var hbs = require('handlebars');
 
-connection.on('connect', function(err) {
-  console.log("Connected");
-});
+// connection.on('connect', function(err) {
+//   console.log("Connected");
+// });
 
 app.set("view engine", "hbs");
 
 app.get("/index", function(req, res){
-  res.render("views/index.hbs");
-})
+  res.render("index.hbs");
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
