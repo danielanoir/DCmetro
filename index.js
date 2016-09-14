660e66e7dac1985b16a08b297a610f6a3d7e0bc4
Daniela Noir <daniela@danielanoir.com>
2016-09-13 20:47:12 -0400

    first commit

--- index.js
+++ index.js
@@ -2,22 +2,57 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var config = require('../config/uber_config');
-var Connection = require('tedious').Connection;
+// var Connection = require('tedious').Connection;
+var ConnectionPool = require('tedious-connection-pool');
 var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
 var data = new String();
 var json = new Object();
-var connection = new Connection(config.db)
+// var connection = new Connection(config.db)
 
-connection.on('connect', function(err) {
-  console.log("Connected");
+var poolConfig = {
+  min: 2,
+  max: 4,
+  log: true
+};
+
+var pool = new ConnectionPool(poolConfig, config.db);
+pool.on('error', function(err) {
+  console.error(err);
 });
 
-app.set("view engine", "hbs");
+pool.acquire(function(err, connection) {
+  if (err) {
+    console.err(err);
+    return;
+  }
 
-app.get("/index", function(req, res){
-  res.render("views/index.hbs");
-})
+  app.get("/coordinates", function(req, res){
+          getSqlData(function(err, json) {
+        res.send(json);
+      });
+  });
+
+function getSqlData(callback) {
+  var sql = 'select station, x, y from mtr.coordinates for json path'
+  var request = new Request(sql, function(err, rowCount) {
+    if (err) {
+      return callback(err);
+    }
+    json = JSON.parse(data);
+    callback(null, json)
+    data = "";
+    console.log('rowCount: ' + rowCount);
+    connection.release();
+  });
+  request.on('row', function(columns) {
+      columns.forEach(function(column) {
+          data += column.value;
+      });
+    });
+  connection.execSql(request);
+  }
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -28,33 +63,3 @@ app.use(function(req, res, next) {
 http.listen(5000, function() {
   console.log("listening on 5000");
 });
-
-app.get("/coordinates", function(req, res){
-  connection = new Connection(config.db);
-  connection.on('connect', function(err) {
-      console.log("Connected for GET");
-        getSqlData(connection, function(err, json) {
-      res.send(json);
-    });
-  });
-});
-
-
-function getSqlData(connection, callback) {
-  var sql = "select station, x, y from mtr.coordinates for json path"
-  var request = new Request(sql, function(err, rowCount) {
-    if (err) {
-      return callback(err);
-    }
-    json = JSON.parse(data);
-    callback(null, json)
-    json = {};
-    connection.close();
-  });
-  request.on('row', function(columns) {
-      columns.forEach(function(column) {
-          data += column.value;
-      });
-  });
-connection.execSql(request);
-}
