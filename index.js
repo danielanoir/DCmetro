00580f65547f1bd4aec9ed418ebea139cd6a17ca
Daniela Noir <daniela@danielanoir.com>
2016-11-15 23:24:39 -0500

    push

--- index.js
+++ index.js
@@ -74,7 +74,6 @@ function getSqlData(origId, callback) {
   }
 });
 
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
